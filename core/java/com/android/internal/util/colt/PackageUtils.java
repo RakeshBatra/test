crdroidandroid/android_frameworks_base

Add custom QS header images [1/2]
Change-Id: I55d602f5ddba093e835d817f55a22063ecae082d
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/util/crdroid/PackageUtils.java
@@ -1,0 +1,50 @@
+/*
+ *  Copyright (C) 2014 The OmniROM Project
+ *            (C) 2017 crDroid Android Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.internal.util.crdroid;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class PackageUtils {
+
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/crdroid/StaticHeaderProvider.java
@@ -1,0 +1,130 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *            (C) 2017 crDroid Android Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.crdroid;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.SparseArray;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import com.android.systemui.R;
+import com.android.internal.util.crdroid.PackageUtils;
+
+public class StaticHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "StaticHeaderProvider";
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+    private Resources mRes;
+    private String mImage;
+    private String mPackageName;
+
+    public StaticHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "static";
+    }
+
+    @Override
+    public void settingsChanged() {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        String imageUrl = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+
+        if (imageUrl != null && customHeader) {
+            int idx = imageUrl.indexOf("/");
+            if (idx != -1) {
+                String[] parts = imageUrl.split("/");
+                mPackageName = parts[0];
+                mImage = parts[1];
+                loadHeaderImage();
+            }
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged();
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void loadHeaderImage() {
+        if (DEBUG) Log.i(TAG, "Load header image " + " " + mPackageName + " " + mImage);
+
+        try {
+            PackageManager packageManager = mContext.getPackageManager();
+            mRes = packageManager.getResourcesForApplication(mPackageName);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load icon pack " + mPackageName, e);
+            mRes = null;
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        if (mRes == null) {
+            return null;
+        }
+
+        try {
+            return mRes.getDrawable(mRes.getIdentifier(mImage, "drawable", mPackageName), null);
+        } catch(Resources.NotFoundException e) {
+            Log.w(TAG, "No drawable found for " + now +" in " + mPackageName);
+        }
+        return null;
+    }
+}

RakeshBatra/test

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/com/android/internal/util/colt/PackageUtils.java
@@ -1,0 +1,50 @@
+/*
+ *  Copyright (C) 2014 The OmniROM Project
+ *            (C) 2017 Colt-OS Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.internal.util.colt;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class PackageUtils {
+
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/colt/omnistyle/StaticHeaderProvider.java
@@ -1,0 +1,130 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *            (C) 2017 Colt-OS Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.colt.omnistyle;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.SparseArray;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import com.android.systemui.R;
+import com.android.internal.util.colt.PackageUtils;
+
+public class StaticHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "StaticHeaderProvider";
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+    private Resources mRes;
+    private String mImage;
+    private String mPackageName;
+
+    public StaticHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "static";
+    }
+
+    @Override
+    public void settingsChanged() {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        String imageUrl = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+
+        if (imageUrl != null && customHeader) {
+            int idx = imageUrl.indexOf("/");
+            if (idx != -1) {
+                String[] parts = imageUrl.split("/");
+                mPackageName = parts[0];
+                mImage = parts[1];
+                loadHeaderImage();
+            }
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged();
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void loadHeaderImage() {
+        if (DEBUG) Log.i(TAG, "Load header image " + " " + mPackageName + " " + mImage);
+
+        try {
+            PackageManager packageManager = mContext.getPackageManager();
+            mRes = packageManager.getResourcesForApplication(mPackageName);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load icon pack " + mPackageName, e);
+            mRes = null;
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        if (mRes == null) {
+            return null;
+        }
+
+        try {
+            return mRes.getDrawable(mRes.getIdentifier(mImage, "drawable", mPackageName), null);
+        } catch(Resources.NotFoundException e) {
+            Log.w(TAG, "No drawable found for " + now +" in " + mPackageName);
+        }
+        return null;
+    }
+}