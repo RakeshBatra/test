DirtyUnicorns/android_packages_apps_Settings

Improvements to dashboard suggestions
- Change the logic
- Users might get confused so adding proper summaries for enable and disable suggestions

http://i.imgur.com/OJv71IN.png
http://i.imgur.com/ylojEgY.png

Change-Id: I6e3f47ecb7a54f51a84b70bae0cdb16d5aef6731

src/com/android/settings/dashboard/DashboardAdapter.java
@@ -92,20 +92,20 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
 
     public List<Tile> getSuggestions() {
         if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DISABLE_SUGGESTIONS, 0) == 1)) {
-             return null;
-        } else {
+                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
              return mSuggestions;
+        } else {
+             return null;
         }
     }
 
     public void setSuggestions(List<Tile> suggestions) {
         if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DISABLE_SUGGESTIONS, 0) == 1)) {
-             mSuggestions = null;
+                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+             mSuggestions = suggestions;
              recountItems();
         } else {
-             mSuggestions = suggestions;
+             mSuggestions = null;
              recountItems();
         }
     }

RakeshBatra/test

Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/settings/dashboard/DashboardAdapter.java
@@ -108,10 +108,10 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
 
     public List<Tile> getSuggestions() {
         if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DISABLE_SETTINGS_SUGGESTIONS, 0) == 1)) {
-             return null;
-        } else {
+                    Settings.System.DISABLE_SETTINGS_SUGGESTIONS, 1) == 1)) {
              return mSuggestions;
+        } else {
+             return null;
         }
     }
 
@@ -120,11 +120,11 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
         mCategories = categories;
 
         if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DISABLE_SETTINGS_SUGGESTIONS, 0) == 1)) {
-             mSuggestions = null;
+                    Settings.System.DISABLE_SETTINGS_SUGGESTIONS, 1) == 1)) {
+             mSuggestions = suggestions;
              recountItems();
         } else {
-             mSuggestions = suggestions;
+             mSuggestions = null;
              recountItems();
         }
 